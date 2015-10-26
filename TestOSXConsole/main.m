9096f1a45f8183e1f0bc9cb6676eb407f01e7043
212419672 <john.kruger@ge.com>
2015-10-26 09:24:12 -0700

    Initial Commit

---
+++ TestOSXConsole/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestOSXConsole
+//
+//  Created by Kruger, John (GE Corporate) on 26/10/15.
+//  Copyright © 2015 Kruger, John (GE Corporate). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
